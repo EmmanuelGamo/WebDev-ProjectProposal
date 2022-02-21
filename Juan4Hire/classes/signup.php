da900a0f0722edb8918d736198aeaecc23eaf722
EmmanuelGamo <58947901+EmmanuelGamo@users.noreply.github.com>
2022-02-21 21:01:59 +0800

    Add files via upload
    Sign up page w/ working database

---
+++ Juan4Hire/classes/signup.php
@@ -0,0 +1,99 @@
+<?php
+
+class Signup
+{
+    private $error = "";
+    function function_alert($message) 
+    { 
+        echo "<script>alert('$message');</script>";
+    }
+
+    public function evaluate($data)
+    {
+        foreach ($data as $key => $value)
+        {
+            if(empty($value))
+            {
+                $this->error = $this->error . $key . " is empty!" ;
+            }
+
+            if($key == "email")
+            {
+                if (!filter_var($value, FILTER_VALIDATE_EMAIL))
+                {
+                    $this->error = $this->error . "invalid email address!";
+                }
+
+            }
+            if($key == "first_name")
+            {
+                if (is_numeric($value))
+                {
+                    $this->error = $this->error . "names can't be a number!";
+                }
+
+            }
+            if($key == "last_name")
+            {
+                if (is_numeric($value))
+                {
+                    $this->error = $this->error . "names can't be a number!";
+                }
+
+            }
+            if ($key == "password")
+            {   
+                if(strlen($value) < 6)
+                {
+
+					$this->error = $this->error . "Password must be atleast 6 characters long";
+				}
+                if ($_POST['password'] != $_POST['con_password'])
+               {
+                {
+                    $this->error = $this->error . "The password confirmation does not match";
+                }
+               }
+            }
+        }
+        
+        if($this->error == "")
+        {
+        //no error
+            $this->create_user($data);
+            
+        }else
+        {
+            return $this->error;
+        }
+    }
+
+    public function create_user($data)
+    {
+        $first_name = ucfirst($data['first_name']);
+        $last_name = ucfirst($data['last_name']);
+        $email = $data['email'];
+        $password = $data['password'];
+        $url_address = strtolower($first_name) ."." . strtolower($last_name);
+        $userid = $this->create_userid();
+
+        $query = "insert into users(userid, first_name , last_name, email, password, url_address)
+                        values('$userid','$first_name','$last_name','$email','$password','$url_address')";
+
+        $DB = new Database();
+        $DB->save($query);
+
+    }
+
+    private function create_userid()
+    {
+            $length = rand(4,19);
+            $number = "";
+            for ($i=0; $i < $length; $i++)
+            {
+                $new_rand = rand(0,9);
+                $number = $number . $new_rand;
+            }
+            return $number;
+    }
+}
---
+++ Juan4Hire/signup.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<?php
+  include("classes/connect.php");
+  include("classes/signup.php");
+  
+  function function_alert($message) 
+  { 
+      echo "<script>alert('$message');</script>";
+  }
+
+  $first_name = "";
+  $last_name = "";
+  $email = "";
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST') 
+  {
+    $signup = new Signup();
+    $result = $signup->evaluate($_POST);
+    if ($result != "")
+    {
+      function_alert("$result");
+    }
+    else
+    {
+       echo(
+         "<script LANGUAGE='JavaScript'>
+           window.alert('Your account has been successfully created');
+         window.location.href='login.php';
+        </script>");
+       die;
+    }
+
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+  }
+
+?> 
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel = "stylesheet" type = "text/css" href="signup.css">
+</head>
+
+<body>
+    <form method="post" action ="">
+      <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+      <div id="container">
+        <div class="main-content">
+          <div class="logo">
+            <img src= "J4Hlogo.png" />
+          </div>
+          <div class="signup">
+            <input value="<?php echo $first_name ?>" type="text" name= "first_name" id="text" placeholder="First Name" class="Fname" />
+            <input value="<?php echo $last_name ?>" type="text" name="last_name" id="text" placeholder="Last Name" class="Lname" />
+            <input type="text" name="email" id="text" placeholder="Email" class="Email" />
+            <input type="password" name="password" id="text" placeholder="Password" class="pass" />
+            <input type="password" name="con_password"  id="text" placeholder="Confirm Password" class="pass" />
+            <input type="submit" id="button" value="Sign Up" class="btn" />
+          </div>
+        </div>
+      </div>
+    </form>
+</body>
+</html>
